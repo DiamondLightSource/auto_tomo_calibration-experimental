626d0abb59329452f34ae87ed8be3ebd9ec63141
tomasaidukas <tomasaiduk@gmail.com>
2015-06-29 17:16:17 +0100

    can repair the radii plots to remove anomalies

--- Analysis_Cluster/plot_radii.py
+++ Analysis_Cluster/plot_radii.py
@@ -6,19 +6,20 @@ radii = []
 for i in range(0,360,10):
     radii.append(np.load('/dls/tmp/jjl36382/radii/radii%03i.npy' % i))
     
-# Remove the anomalous radii
-"""radii_med = np.median(radii)
-one_std_dev = np.std(radii)
-for i in xrange(0,360,10):
-    for j in xrange(0,181):
-        if abs(radii[i, j] - radii_med) >= one_std_dev:
-            radii[i, j] = radii_med"""
         
 radii_np = np.zeros((360,181))
 for i in range(36):
     radii_np[i*10:i*10+10,:] = radii[i]
 
-
+# Remove the anomalous radii
+radii_med = np.median(radii_np)
+one_std_dev = np.std(radii_np)
+for i in xrange(0,360):
+    for j in xrange(0,181):
+        if abs(radii_np[i, j] - radii_med) > one_std_dev:
+            print abs(radii_np[i, j] - radii_med)
+            radii_np[i, j] = radii_med
+            
 
 # Plot
 
