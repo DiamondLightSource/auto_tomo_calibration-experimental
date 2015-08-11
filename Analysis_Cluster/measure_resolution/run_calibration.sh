48790e1784cbafe2c4abc2019a303fd765e4f233
tomasaidukas <tomasaiduk@gmail.com>
2015-08-11 09:08:21 +0100

    use in the cluster

--- Analysis_Cluster/measure_resolution/run_calibration.sh
+++ Analysis_Cluster/measure_resolution/run_calibration.sh
@@ -14,18 +14,26 @@ cd /dls/tmp/jjl36382/resolution1/logs
 #stop=2160
 #step=10
 
+#folder_start = "./huge_contrast/"
+#name = folder_start + "data/sino_%05i.tif"
+#label_name = folder_start + "label/analytical%i.png"
+#results = folder_start + "results/result%i.txt"
+#sorted = folder_start + "sorted/"
+#plots = folder_start + "plots/"
+
 mkdir /dls/tmp/jjl36382/resolution1
 mkdir /dls/tmp/jjl36382/resolution1/logs
 mkdir /dls/tmp/jjl36382/resolution1/results
-mkdir /dls/tmp/jjl36382/resolution1/spheres
-mkdir /dls/tmp/jjl36382/resolution1/analysis
+mkdir /dls/tmp/jjl36382/resolution1/sorted
+mkdir /dls/tmp/jjl36382/resolution1/plots
+mkdir /dls/tmp/jjl36382/resolution1/label
 cd /dls/tmp/jjl36382/resolution1/logs
 
-homepath="${HOME}/auto_tomo_calibration-experimental/Analysis_Cluster/measure_resolution"
-datapath="/dls/tmp/tomas_aidukas/scans_july16/cropped/50867/image_%05i.tif"
-resultspath="/dls/tmp/jjl36382/resolution1/results"
-spherepath="/dls/tmp/jjl36382/resolution1/spheres"
-analysispath="/dls/tmp/jjl36382/complicated_data/resolution1/analysis"
+#homepath="${HOME}/auto_tomo_calibration-experimental/Analysis_Cluster/measure_resolution"
+#datapath="/dls/tmp/tomas_aidukas/scans_july16/cropped/50867/image_%05i.tif"
+#resultspath="/dls/tmp/jjl36382/resolution1/results"
+#spherepath="/dls/tmp/jjl36382/resolution1/spheres"
+#analysispath="/dls/tmp/jjl36382/complicated_data/resolution1/analysis"
 
 # ENTER START FILE NUMBER + 1 AND END NUMBER +1
 start=1
@@ -38,20 +46,20 @@ tolerance=2
 # Merge tiffs ------------------------------------------------------------------------------------------------------
 # Do this independently of other processes
 holder="-N merge"
-#qsub $holder -pe smp 2 -j y -t 1 $homepath/merge.sh $resultspath $datapath $start $stop
+qsub $holder -pe smp 2 -j y -t 1 $homepath/merge.sh $datapath
 
 # Detect circles ------------------------------------------------------------------------------------------------------
 holder="-N detect"
-qsub $holder -pe smp 2 -j y -t $start-$stop:$step -tc 20 $homepath/detector.sh $datapath $resultspath/out%05i.dat
+#qsub $holder -pe smp 2 -j y -t $start-$stop:$step -tc 20 $homepath/detector.sh $datapath $resultspath/out%05i.dat
 
 # Analyse areas ------------------------------------------------------------------------------------------------------
 holder="-hold_jid detect -N analyse"
 #qsub $holder -pe smp 2 -j y -t 1 $homepath/sort_centres.sh $start $stop $step $resultspath/out%05i.dat $resultspath
-$homepath/sort_centres.sh $start $stop $step $resultspath/out%05i.dat $resultspath
+#$homepath/sort_centres.sh $start $stop $step $resultspath/out%05i.dat $resultspath
 
 # Plot images based on centres ---------------------------------------------------------------------------------------
-$homepath/plot_image.sh $start $stop $step $resultspath/out%05i.dat $resultspath
+#$homepath/plot_image.sh $start $stop $step $resultspath/out%05i.dat $resultspath
 
 # Get resolution ------------------------------------------------------------------------------------------------------
 holder="-hold_jid merge -N resolution"
-qsub $holder -pe smp 2 -j y -t 1 -tc 10 $homepath/find_contacts.sh $resultspath $tolerance
+#qsub $holder -pe smp 2 -j y -t 1 -tc 10 $homepath/find_contacts.sh $resultspath $tolerance
