6f52bb5ee373dca7edec8bbb7bea8bd49c416e30
Tomas Aidukas <2035231a@student.gla.ac.uk>
2015-09-04 21:09:04 +0100

    Update calculate_resolution.sh

--- measure_resolution/calculate_resolution.sh
+++ measure_resolution/calculate_resolution.sh
@@ -4,6 +4,8 @@ module load global/cluster
 # SPECIFY DATA PATH AND OUTPUT PATH 
 datapath="/dls/tmp/tomas_aidukas/new_recon_steel/50867/reconstruction/r_2015_0825_195403_images/image_%05i.tif"
 outputpath="/dls/tmp/jjl36382/suppresed_50867"
+# HOMEPATH - DIRECTORY WHICH CONTAINS ALL THE PYTHON/SHELL SCRIPTS
+homepath="${HOME}/auto_tomo_calibration-experimental/measure_resolution"
 
 # ENTER STARTING FILE NAME NUMBER +1 AND END NUMBER +1
 # THE IMAGE NAME FORMAT IS IMAGE_#####.TIF
@@ -41,8 +43,6 @@ cd $outputpath/logs
 
 #############################################################
 # VARIABLES TO STORE PATH NAMES USED IN PYTHON SCRIPTS
-# HOMEPATH - DIRECTORY WHICH CONTAINS ALL THE PYTHON/SHELL SCRIPTS
-homepath="${HOME}/auto_tomo_calibration-experimental/measure_resolution"
 
 # RESULTS IS THE FOLDER CONTAINING CIRCLE PERIMETERS AND THEIR CENTRES
 # IF SEGMENTATION FAILS, THE TEXT FILES CAN HAVE CENTROIDS WRITTEN MANUALLY
@@ -72,52 +72,17 @@ holder="-hold_jid job1 -N job2"
 #qsub $holder -pe smp 2 -j y -t 1 $homepath/sort_centres.sh $start $stop $step $resultspath/out%05i.dat $resultspath $homepath
 
 
-### CAN BE USED FOR SINGLE SPHERE SEGMENTATION IF NEEDED ###
-### NOT USABLE ANYMORE, SINCE THERE ARE BETTER SOLUTIONS ###
-### SEGMENT THE SPHERE APPLY EDM AND STORE CENTROIDS ###########
-holder="-hold_jid job2 -N job3"
-#qsub $holder -pe smp 2 -j y -t 1 -tc 10 $homepath/selector_loop.sh $spherepath $datapath $resultspath $homepath 
-
-
-# FIND CONTACT POINTS AND MEASURE RESOLUTION AROUND THEM 
+# FIND CONTACT POINTS AND MEASURE RESOLUTION AROUND THEM
+# COMMENT IT OUT IF NOT IN USE
 #$homepath/find_contacts.sh $plotspath $resultspath/ $datapath $start $stop $homepath $tolerance $window_size
 
 
 
 
-# QSUB WAS NOT USED SINCE IT DOES NOT HAVE A DISPLAY - CAN'T PLOT
-datapath="/dls/tmp/tomas_aidukas/new_recon_steel/50873/recon_noringsup/r_2015_0825_200208_images/image_%05i.tif"
-outputpath="/dls/tmp/jjl36382/50873"
-# RESULTS STORE THE DATA OF THE CIRCLE PERIMETERS AND THEIR CENTRES
-resultspath=$outputpath"/results"
-# PLOTS CONTAIN THE REGIONS OF POINTS OF CONTACT
-plotspath=$outputpath"/plots"
 
-# FIND CONTACT POINTS AND MEASURE RESOLUTION AROUND THEM
-$homepath/find_contacts.sh $plotspath $resultspath/ $datapath $start $stop $homepath $tolerance $window_size
-
-
-datapath="/dls/tmp/tomas_aidukas/new_recon_steel/50880/recon_noringsup/r_2015_0825_200209_images/image_%05i.tif"
-outputpath="/dls/tmp/jjl36382/50880"
-# RESULTS STORE THE DATA OF THE CIRCLE PERIMETERS AND THEIR CENTRES
-resultspath=$outputpath"/results"
-# PLOTS CONTAIN THE REGIONS OF POINTS OF CONTACT
-plotspath=$outputpath"/plots"
-
-# FIND CONTACT POINTS AND MEASURE RESOLUTION AROUND THEM 
-$homepath/find_contacts.sh $plotspath $resultspath/ $datapath $start $stop $homepath $tolerance $window_size
-
-
-
-datapath="/dls/tmp/tomas_aidukas/new_recon_steel/50867/recon_noringsup/r_2015_0825_200207_images/image_%05i.tif"
-outputpath="/dls/tmp/jjl36382/50867"
-# RESULTS STORE THE DATA OF THE CIRCLE PERIMETERS AND THEIR CENTRES
-resultspath=$outputpath"/results"
-# PLOTS CONTAIN THE REGIONS OF POINTS OF CONTACT
-plotspath=$outputpath"/plots"
-
-# FIND CONTACT POINTS AND MEASURE RESOLUTION AROUND THEM
-$homepath/find_contacts.sh $plotspath $resultspath/ $datapath $start $stop $homepath $tolerance $window_size
-
-
+### CAN BE USED FOR SINGLE SPHERE SEGMENTATION IF NEEDED ###
+### NOT USABLE ANYMORE, SINCE THERE ARE BETTER SOLUTIONS ###
+### SEGMENT THE SPHERE APPLY EDM AND STORE CENTROIDS ###########
+holder="-hold_jid job2 -N job3"
+#qsub $holder -pe smp 2 -j y -t 1 -tc 10 $homepath/selector_loop.sh $spherepath $datapath $resultspath $homepath 
 
